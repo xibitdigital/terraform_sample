bd3a41c3a7d5825e954812f817f88f2274c91952
Daniele Sangalli <danielesangalli@Danieles-MacBook-Pro-M1.local>
2022-04-27 01:13:56 +0100

    adding vpc to isntance

--- sample_instance/main.tf
+++ sample_instance/main.tf
@@ -3,10 +3,10 @@ resource "aws_instance" "foo" {
   ami           = var.ec2_ami
   instance_type = var.ec2_instance_type
 
-#   network_interface {
-#     network_interface_id = aws_network_interface.foo.id
-#     device_index         = 0
-#   }
+  network_interface {
+    network_interface_id = aws_network_interface.foo.id
+    device_index         = 0
+  }
 
   credit_specification {
     cpu_credits = "unlimited"
@@ -14,5 +14,34 @@ resource "aws_instance" "foo" {
 
   tags = {
     "Name" = var.ec2_instance_name
+  }
+}
+
+
+# with vpc
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "172.16.0.0/16"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "172.16.10.0/24"
+  availability_zone = "us-west-2a"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
+
+resource "aws_network_interface" "foo" {
+  subnet_id   = aws_subnet.my_subnet.id
+  private_ips = ["172.16.10.100"]
+
+  tags = {
+    Name = "primary_network_interface"
   }
 }
